52a78aba7c7f1eb2de42206ec341465100f7bc58
Nthingtech <store.nthing@gmail.com>
2023-10-13 12:57:10 -0300

    Alinhamento do Button e add key

--- app/src/main/java/com/example/academiatech/MainActivity.java
+++ app/src/main/java/com/example/academiatech/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void openActivityLogin(View view) {
         Intent intent = new Intent(this, MainActivityLogin.class);
-        startActivity(intent); //TODO TESTE GITHUB WEBHOOKS
+        startActivity(intent);
     }
 
 }
