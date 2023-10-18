0260d0f1b044052fd1c8549a34a291dd3ccb921a
Nthingtech <store.nthing@gmail.com>
2023-10-17 22:23:29 -0300

    Change color RadioButton

--- app/src/main/java/com/example/academiatech/MainActivityRegister.java
+++ app/src/main/java/com/example/academiatech/MainActivityRegister.java
@@ -2,7 +2,10 @@ package com.example.academiatech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivityRegister extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivityRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_register);
+        RadioButton radioButton = findViewById(R.id.radioButtonMasc);
+        ColorStateList colorStateList = ColorStateList.valueOf(Color.BLACK);
+        radioButton.setButtonTintList(colorStateList);
+        RadioButton radioButton1 = findViewById(R.id.radioButtonFem);
+        ColorStateList colorStateList1 = ColorStateList.valueOf(Color.BLACK);
+        radioButton1.setButtonTintList(colorStateList1);
     }
 }
