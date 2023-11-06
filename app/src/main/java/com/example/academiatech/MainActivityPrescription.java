ef555ab793d18fe0fa28f28e4f19cf48e1b1deb0
Nthingtech <store.nthing@gmail.com>
2023-11-05 22:13:57 -0300

    Dropdown search cliente

--- app/src/main/java/com/example/academiatech/MainActivityPrescription.java
+++ app/src/main/java/com/example/academiatech/MainActivityPrescription.java
@@ -11,4 +11,6 @@ public class MainActivityPrescription extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_prescription);
     }
+
+    //TODO 6:42 MIN
 }
