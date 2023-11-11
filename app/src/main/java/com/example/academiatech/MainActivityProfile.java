95dbbbb178eb2699a86a9a71a5eadcd599e1254c
Nthingtech <store.nthing@gmail.com>
2023-11-11 20:34:18 -0300

    Create Profile layout

---
+++ app/src/main/java/com/example/academiatech/MainActivityProfile.java
@@ -0,0 +1,14 @@
+package com.example.academiatech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_profile);
+    }
+}
