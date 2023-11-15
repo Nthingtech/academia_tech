36adbdcdfd731500b44943c2eb07e3db9c472a09
Nthingtech <store.nthing@gmail.com>
2023-11-15 09:27:33 -0300

    Fix take a picture

--- app/src/main/java/com/example/academiatech/MainActivityProfile.java
+++ app/src/main/java/com/example/academiatech/MainActivityProfile.java
@@ -1,29 +1,19 @@
 package com.example.academiatech;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.ImageDecoder;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivityProfile extends AppCompatActivity {
 
     CircleImageView profile_image;
-
-
 
 
 
@@ -34,39 +24,25 @@ public class MainActivityProfile extends AppCompatActivity {
 
         profile_image = findViewById(R.id.profile_image);
 
-        profile_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                /*Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);*/
-                takePicture.launch(intent);
-            }
+        profile_image.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            activityResultLauncher.launch(intent);
         });
+
 
     }
 
-    ActivityResultLauncher<Intent> takePicture = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        if (data != null) {
-                            Uri uri = data.getData();
-                            try {
-                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
-                                Bitmap bitmap = ImageDecoder.decodeBitmap(source);
-                                CircleImageView imageView = findViewById(R.id.profile_image);
-                                imageView.setImageBitmap(bitmap);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null){
+                    Bundle bundle = result.getData().getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    profile_image.setImageBitmap(bitmap);
                 }
-            });
+            }
+    );
+
 
 
 }
