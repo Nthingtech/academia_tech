77cb23212659e947f54fce0921ba156bea8e7ceb
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2023-12-06 18:18:14 -0300

    CALC

--- app/src/main/java/com/example/academiatech/MainActivityCalcFct.java
+++ app/src/main/java/com/example/academiatech/MainActivityCalcFct.java
@@ -14,6 +14,10 @@ public class MainActivityCalcFct extends AppCompatActivity {
     private EditText editTextfreq3;
     private EditText editTextIdade;
 
+    private EditText editTextMedia;
+    private EditText editTextFct1;
+    private EditText editTextFct2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +26,14 @@ public class MainActivityCalcFct extends AppCompatActivity {
         editTextfreq2 = findViewById(R.id.editTextfreq2);
         editTextfreq3 = findViewById(R.id.editTextfreq3);
         editTextIdade = findViewById(R.id.editTextIdade);
+        editTextMedia = findViewById(R.id.editTextMedia);
+        editTextFct1 = findViewById(R.id.editTextFct1);
+        editTextFct2 = findViewById(R.id.editTextFct2);
+
+
     }
 
-    public void fct(View view) {
+    public void fct1(View view) {
         double fRepo1, fRepo2, fRepo3, idade, fct1;
         fRepo1 = Double.parseDouble(editTextfreq1.getText().toString());
         fRepo2 = Double.parseDouble(editTextfreq2.getText().toString());
@@ -35,9 +44,21 @@ public class MainActivityCalcFct extends AppCompatActivity {
         double repo = (fRepo1 + fRepo2 + fRepo3) / 3;
         double reserv = max - repo;
         fct1 = reserv * 0.8 + repo;
+        editTextFct1.setText(Double.toString(fct1));
+    }
 
+    public void fct2(View view) {
+        double fRepo1, fRepo2, fRepo3, idade, fct1;
+        fRepo1 = Double.parseDouble(editTextfreq1.getText().toString());
+        fRepo2 = Double.parseDouble(editTextfreq2.getText().toString());
+        fRepo3 = Double.parseDouble(editTextfreq3.getText().toString());
+        idade = Double.parseDouble(editTextIdade.getText().toString());
 
-
+        double max = 208 - (0.7 * idade);
+        double repo = (fRepo1 + fRepo2 + fRepo3) / 3;
+        double reserv = max - repo;
+        fct1 = reserv * 0.9 + repo;
+        editTextFct2.setText(Double.toString(fct1));
     }
 
     public void openActivityPrincipal(View view) {
